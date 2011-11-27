7f92c23e6965503db16ae39086e0b6295aa39cb8
Conall O Maitiu <conall.o.maitiu@gmail.com>
2011-11-27 21:57:18 +0000

    Added rules to firefox and pidgin in rc.lua, to keep firefox to tag 2 and pidgin to tag 3, and also to get pidgin to ignore size hints (if I'm using dual monitors and pidgin is on the border between the two and I squeeze it against the edge too much, it overlaps onto the other monitor. Ignoring size hints prevents that).

--- rc.lua
+++ rc.lua
@@ -337,11 +337,12 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
-   { rule = { class = "pidgin" },
-      properties = { size_hints_honor = false } },
+   { rule = { class = "Pidgin" },
+      properties = { size_hints_honor = false,
+		     tag = tags[1][3]} },
     -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
+   { rule = { class = "Firefox" },
+     properties = { tag = tags[1][2] } },
 }
 -- }}}
 
